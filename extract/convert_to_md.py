27796c9ce445cc15c471278bb59aff7e76c49625
seblful <alesha.chimba@gmail.com>
2024-08-28 15:44:54 +0300

    [EDIT] convert with classes

--- extract/convert_to_md.py
+++ extract/convert_to_md.py
@@ -1,19 +1,9 @@
 import os
-import io
-from PIL import Image
 
-import numpy as np
-import cv2
-import pypdfium2 as pdfium
-
-from marker.models import load_all_models
-from marker.convert import convert_single_pdf
-from marker.output import save_markdown
-
+from processors import PDFProcessor
 
 # For some reason, transformers decided to use .isin for a simple op, which is not supported on MPS
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]
-
 
 # Paths
 HOME = os.getcwd()
@@ -22,126 +12,17 @@ RAW_DIR = os.path.join(DATA_DIR, "raw")
 INPUT_DIR = os.path.join(DATA_DIR, "inputs")
 OUTPUT_DIR = os.path.join(DATA_DIR, "outputs")
 
-LANGS = ["ru", "en"]
-
-
-def remove_color(img: np.array,
-                 lower_blue=np.array([115, 150, 70]),
-                 upper_blue=np.array([130, 255, 255])) -> None:
-    # Convert to HSV color space
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # Create a mask for blue color
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    # Dilate the mask to cover entire pen marks
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations=1)
-
-    # Inpaint the masked region
-    img = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
-
-    return img
-
-
-def clean_image(image: Image.Image,
-                remove_ink: float = True,
-                binarize: float = False,
-                blur: float = False) -> Image.Image:
-    # Convert image to array
-    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-    # Adjust contrast and brightness
-    img = cv2.convertScaleAbs(img,
-                              alpha=3,
-                              beta=15)
-
-    # Binarize image
-    if binarize is True:
-        _, img = cv2.threshold(
-            img, 150, 255, cv2.THRESH_BINARY)
-
-    # Blur
-    if blur is True:
-        img = cv2.GaussianBlur(img, (3, 3), 0)
-
-    # Remove ink
-    if remove_ink is True:
-        img = remove_color(img=img)
-
-    return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-
-def preprocess_pdf(pdf_path: str) -> None:
-    # Open pdf
-    raw_pdf = pdfium.PdfDocument(pdf_path)
-    version = raw_pdf.get_version()
-
-    # Create new pdf
-    input_pdf = pdfium.PdfDocument.new()
-
-    # Iterate
-    for page in raw_pdf:
-        # Get width and height of the page
-        width, height = page.get_size()
-
-        # Retrieve image
-        bitmap = page.render(scale=3,
-                             rotation=0)
-        image = bitmap.to_pil()
-
-        # Clean image
-        image = clean_image(image=image,
-                            remove_ink=True,
-                            binarize=False,
-                            blur=False)
-
-        # Add image to the new pdf
-        pdf_image = pdfium.PdfImage.new(input_pdf)
-        bitmap = pdfium.PdfBitmap.from_pil(image)
-        image.close()
-        pdf_image.set_bitmap(bitmap)
-        bitmap.close()
-
-        pdf_image.set_matrix(pdfium.PdfMatrix().scale(width, height))
-        page = input_pdf.new_page(width, height)
-        page.insert_obj(pdf_image)
-        page.gen_content()
-
-        # Close
-        pdf_image.close()  # no-op
-        page.close()
-
-    input_pdf.save(os.path.join(
-        INPUT_DIR, os.path.basename(pdf_path)), version=version)
-
-    # Close pdf
-    raw_pdf.close()
-    input_pdf.close()
-
 
 def main() -> None:
-    # Load models
-    model_lst = load_all_models()
-
-    for pdf_name in os.listdir(INPUT_DIR):
-        # Check if it was converted before
-        if not os.path.exists(os.path.join(OUTPUT_DIR, os.path.splitext(pdf_name)[0])):
-            full_pdf_path = os.path.join(INPUT_DIR, pdf_name)
-
-            # Convert pdf
-            full_text, images, out_meta = convert_single_pdf(
-                full_pdf_path, model_lst, langs=LANGS,  ocr_all_pages=True)
-
-            # Save markdown
-            subfolder_dir = save_markdown(
-                OUTPUT_DIR, pdf_name, full_text, images, out_meta)
-            print(f"Saved markdown to the '{
-                os.path.basename(subfolder_dir)}' folder.")
-
-            break
+    pdf_processor = PDFProcessor(raw_dir=RAW_DIR,
+                                 input_dir=INPUT_DIR,
+                                 output_dir=OUTPUT_DIR,
+                                 alpha=3, beta=15,
+                                 remove_ink=True,
+                                 binarize=False,
+                                 blur=False)
+    pdf_processor.process()
 
 
 if __name__ == "__main__":
-    # preprocess_pdf(pdf_path=os.path.join(RAW_DIR, "ЦТ Биология 2022.pdf"))
     main()
