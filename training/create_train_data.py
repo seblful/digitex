10cf85541f0b5ad0a82a4493c3312230a9ed0139
seblful <alesha.chimba@gmail.com>
2024-08-29 19:26:04 +0300

    [EDIT] script to create data

--- training/create_train_data.py
+++ training/create_train_data.py
@@ -1,9 +1,23 @@
 import os
 import random
 
+from data_creator import DataCreator
 
-def main():
-    pass
+# Paths
+HOME = os.getcwd()
+
+TESTING_DIR = os.path.dirname(HOME)
+RAW_DIR = os.path.join(TESTING_DIR, "raw-data")
+TRAIN_DIR = os.path.join(HOME, "data", "train-data")
+
+
+def main() -> None:
+    # Create DataCreator instance
+    data_creator = DataCreator(raw_data_dir=RAW_DIR,
+                               train_dir=TRAIN_DIR)
+
+    # Create data
+    data_creator.create_train_data(num_images=20)
 
 
 if __name__ == "__main__":
