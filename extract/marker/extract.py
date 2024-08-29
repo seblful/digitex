b7a326a5897247c79e35bdb6e53c6b1f76f35d15
seblful <alesha.chimba@gmail.com>
2024-08-29 18:04:48 +0300

    [EDIT] new raw path

--- extract/marker/extract.py
+++ extract/marker/extract.py
@@ -7,8 +7,11 @@ os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]
 
 # Paths
 HOME = os.getcwd()
+
+TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
+RAW_DIR = os.path.join(TESTING_DIR, "raw-data")
+
 DATA_DIR = os.path.join(HOME, "data")
-RAW_DIR = os.path.join(DATA_DIR, "raw")
 INPUT_DIR = os.path.join(DATA_DIR, "inputs")
 OUTPUT_DIR = os.path.join(DATA_DIR, "outputs")
 
@@ -17,7 +20,7 @@ def main() -> None:
     pdf_processor = PDFProcessor(raw_dir=RAW_DIR,
                                  input_dir=INPUT_DIR,
                                  output_dir=OUTPUT_DIR,
-                                 alpha=3, beta=15,
+                                 alpha=2, beta=10,
                                  remove_ink=True,
                                  binarize=False,
                                  blur=False)
