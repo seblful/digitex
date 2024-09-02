79e6c4054b01eee0e157afd5a54d01ee7e34bd45
seblful <alesha.chimba@gmail.com>
2024-09-02 20:55:59 +0300

    [EDIT] create yolo train data

--- training/yolo/create_train_data.py
+++ training/yolo/create_train_data.py
@@ -5,18 +5,20 @@ from modules.data_creator import DataCreator
 # Paths
 HOME = os.getcwd()
 
-TESTING_DIR = os.path.dirname(HOME)
+TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
 RAW_DIR = os.path.join(TESTING_DIR, "raw-data", "medium")
 TRAIN_DIR = os.path.join(HOME, "data", "train-data")
 
 
 def main() -> None:
     # Create DataCreator instance
-    data_creator = DataCreator(input_dir=RAW_DIR,
-                               train_dir=TRAIN_DIR)
+    data_creator = DataCreator()
 
-    # # Create data
-    # data_creator.create_train_data(num_images=1)
+    # Create data
+    data_creator.create_yolo_train_data(raw_dir=RAW_DIR,
+                                        train_dir=TRAIN_DIR,
+                                        scan_type="color",
+                                        num_images=1)
 
 
 if __name__ == "__main__":
