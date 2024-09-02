bc7ae444e7cfdc799429580aad099ebf2d25c2bd
seblful <alesha.chimba@gmail.com>
2024-09-02 22:06:57 +0300

    [EDIT] extract with module image processor

--- extract/marker/extract.py
+++ extract/marker/extract.py
@@ -1,6 +1,6 @@
 import os
 
-from processors import PDFProcessor
+from extractor import PDFProcessor
 
 # For some reason, transformers decided to use .isin for a simple op, which is not supported on MPS
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]
@@ -9,7 +9,7 @@ os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]
 HOME = os.getcwd()
 
 TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
-RAW_DIR = os.path.join(TESTING_DIR, "raw-data")
+RAW_DIR = os.path.join(TESTING_DIR, "raw-data", "medium")
 
 DATA_DIR = os.path.join(HOME, "data")
 INPUT_DIR = os.path.join(DATA_DIR, "inputs")
@@ -19,12 +19,8 @@ OUTPUT_DIR = os.path.join(DATA_DIR, "outputs")
 def main() -> None:
     pdf_processor = PDFProcessor(raw_dir=RAW_DIR,
                                  input_dir=INPUT_DIR,
-                                 output_dir=OUTPUT_DIR,
-                                 alpha=2, beta=10,
-                                 remove_ink=True,
-                                 binarize=False,
-                                 blur=False)
-    pdf_processor.process()
+                                 output_dir=OUTPUT_DIR)
+    pdf_processor.process(scan_type="color")
 
 
 if __name__ == "__main__":
