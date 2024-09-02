4bcb7fccd0f47081d5ebf41ea2b523c2b1928865
seblful <alesha.chimba@gmail.com>
2024-09-02 20:54:35 +0300

    [EDIT] binarize image

--- modules/processors.py
+++ modules/processors.py
@@ -3,6 +3,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
+import doxapy
+
 
 class ImageProcessor:
     def __init__(self,
@@ -10,15 +12,15 @@ class ImageProcessor:
                  remove_ink: bool = True,
                  binarize: bool = False,
                  blur: bool = False) -> None:
-        # Parameters
-        self.fix_luminance = fix_luminance
-        self.remove_ink = remove_ink
-        self.binarize = binarize
-        self.blur = blur
+        # Scan
+        self.scan_types = ["bw", "grey", "color"]
 
         # Blue remove range
         self.lower_blue = np.array([115, 150, 70])
         self.upper_blue = np.array([130, 255, 255])
+
+        # Binarization
+        self.bin_params = {"window": 75, "k": 0.3}
 
     def remove_color(self,
                      img: np.array) -> None:
@@ -60,7 +62,8 @@ class ImageProcessor:
         return img
 
     def clean_image(self,
-                    image: Image.Image) -> Image.Image:
+                    image: Image.Image,
+                    scan_type: str) -> Image.Image:
         # Convert image to array
         img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
@@ -79,5 +82,45 @@ class ImageProcessor:
         # Remove ink
         if self.remove_ink is True:
             img = self.remove_color(img=img)
+
+        return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    def binarize_image(self,
+                       img: np.array):
+        # Convert image to gray
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Create empty binary image
+        bin_img = np.empty(gray.shape, gray.dtype)
+
+        # Convert the image to binary
+        wan = doxapy.Binarization(doxapy.Binarization.Algorithms.WAN)
+        wan.initialize(gray)
+        wan.to_binary(bin_img, self.bin_params)
+
+        # Convert image back to 3d
+        img = cv2.cvtColor(bin_img, cv2.COLOR_GRAY2BGR)
+
+        return img
+
+    def process(self,
+                image: Image.Image,
+                scan_type: str,
+                remove_ink: bool = True,
+                binarize: bool = True):
+        # Check scan type
+        assert scan_type in self.scan_types, f"Scan type should be in one of {
+            str(self.scan_types)}"
+
+        # Convert image to array
+        img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+        # Remove ink
+        if remove_ink is True:
+            img = self.remove_color(img)
+
+        # Binarize image
+        if binarize is True and scan_type != "old":
+            img = self.binarize_image(img)
 
         return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
