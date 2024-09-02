b4308ef66faeb06e5ce209f2691484c55c7580f3
seblful <alesha.chimba@gmail.com>
2024-09-02 20:55:01 +0300

    [EDIT] delete old methods

--- modules/processors.py
+++ modules/processors.py
@@ -39,52 +39,6 @@ class ImageProcessor:
 
         return img
 
-    def change_luminance(self,
-                         img: np.array) -> None:
-        # Calculate average contrast and brightness
-        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        contrast = np.std(gray_image)
-        brightness = np.mean(gray_image)
-
-        # Find alpha and beta
-        alpha = round(np.log10(contrast) / 1.5, 3)
-        beta = round(255 - brightness, 3)
-
-        # # Print alpha and beta
-        # print(f"Contrast: {contrast}, brightness: {brightness}")
-        # print(f"Alpha: {alpha}, beta: {beta}")
-
-        # Change luminance
-        img = cv2.convertScaleAbs(img,
-                                  alpha=alpha,
-                                  beta=beta)
-
-        return img
-
-    def clean_image(self,
-                    image: Image.Image,
-                    scan_type: str) -> Image.Image:
-        # Convert image to array
-        img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-        if self.fix_luminance is True:
-            img = self.change_luminance(img=img)
-
-        # Binarize image
-        if self.binarize is True:
-            _, img = cv2.threshold(
-                img, 150, 255, cv2.THRESH_BINARY)
-
-        # Blur
-        if self.blur is True:
-            img = cv2.GaussianBlur(img, (3, 3), 0)
-
-        # Remove ink
-        if self.remove_ink is True:
-            img = self.remove_color(img=img)
-
-        return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
     def binarize_image(self,
                        img: np.array):
         # Convert image to gray
