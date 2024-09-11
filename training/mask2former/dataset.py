b768363fdfbdbfa3afa8694dad9d707e85dbdacc
seblful <alesha.chimba@gmail.com>
2024-09-11 22:25:32 +0300

    [EDIT] convert polygon to annotation

--- training/mask2former/dataset.py
+++ training/mask2former/dataset.py
@@ -3,6 +3,12 @@ import shutil
 import json
 import random
 
+from PIL import Image
+
+import numpy as np
+import numpy.typing as npt
+import cv2
+
 
 class DatasetCreator():
     def __init__(self,
@@ -23,9 +29,6 @@ class DatasetCreator():
         self.json_path = os.path.join(raw_dir, 'data.json')
         self.classes_path = os.path.join(raw_dir, 'classes.txt')
 
-        # Annotation creator
-        annotation_creator = AnnotationCreator()
-
         # Data split
         self.train_split = train_split
         self.val_split = 0.6 * (1 - self.train_split)
@@ -35,6 +38,10 @@ class DatasetCreator():
         self.__label2id = None
 
         self.__images_labels_dict = None
+
+        # Annotation creator
+        self.annotation_creator = AnnotationCreator(json_path=self.json_path,
+                                                    label2id=self.label2id)
 
     @property
     def masks_dir(self) -> str:
@@ -135,7 +142,10 @@ class DatasetCreator():
         with open(self.classes_path, 'r') as file:
             # Set the names of the classes
             classes = [i.split('\n')[0] for i in file.readlines()]
-            id2label = {k: v for k, v in enumerate(classes)}
+            id2label = {k: v for k, v in enumerate(classes, start=1)}
+
+        # Set background as 0
+        id2label[0] = "background"
 
         return id2label
 
@@ -143,6 +153,8 @@ class DatasetCreator():
         self.train_dir
         self.val_dir
         self.test_dir
+
+        self.annotation_creator.create_annotations()
 
 
 class PolygonLabel():
@@ -166,18 +178,22 @@ class PolygonLabel():
 
 class AnnotationCreator:
     def __init__(self,
-                 json_path: str) -> None:
+                 json_path: str,
+                 label2id: dict[str, int]) -> None:
         # Paths
         self.json_path = json_path
 
+        # Labels
+        self.label2id = label2id
+
     @staticmethod
     def read_json(json_path) -> dict:
-        with open(json_path) as json_file:
+        with open(json_path, "r", encoding="utf-8") as json_file:
             json_dict = json.load(json_file)
 
         return json_dict
 
-    def __get_polygons(self, task):
+    def __get_polygons(self, task) -> tuple[list, int, int]:
         # Retrieve annotations and results
         annotation = task['annotations'][0]
         result = annotation['result']
@@ -198,18 +214,39 @@ class AnnotationCreator:
                 polygon = PolygonLabel(points=value['points'],
                                        label=value['polygonlabels'][0])
 
-                # Append labels to lists if label is not Stamp
-                if polygon.label != "Stamp":
-                    polygons.append(polygon)
-
-        # Sort polygons
-        polygons.sort(key=lambda x: self.label2id[x.label], reverse=True)
+                # Append polygon to polygons
+                polygons.append(polygon)
 
         return polygons, image_width, image_height
 
+    def __convert_polygons_to_annotation(self,
+                                         polygons,
+                                         image_width,
+                                         image_height) -> npt.NDArray[np.uint8]:
+        # Generate blank annotation
+        annotation = np.zeros((3, image_height, image_width), dtype=np.int32)
+
+        # Iterating through polygons
+        for i, polygon in enumerate(polygons, start=1):
+            # Converts points to relative
+            relative_points = polygon.convert_to_relative(
+                image_width, image_height)
+            relative_points = np.array(relative_points, dtype=np.int32)
+
+            # Fill first channel
+            annotation[0] = cv2.fillPoly(
+                annotation[0], [relative_points], self.label2id[polygon.label])
+            # Fill second channel
+            annotation[1] = cv2.fillPoly(annotation[1], [relative_points], i)
+
+        # # Convert annotation to uint8
+        annotation = annotation.astype(np.uint8)
+
+        return annotation
+
     def create_annotations(self) -> None:
         # Read json_polygon_path
-        json_dict = AnnotationCreator.read_json(self.json_polygon_path)
+        json_dict = AnnotationCreator.read_json(self.json_path)
 
         # Iterating through tasks
         for task in json_dict:
@@ -218,13 +255,15 @@ class AnnotationCreator:
             polygons, image_width, image_height = self.__get_polygons(task)
 
             # Get image path and mask path
-            image_name = os.path.basename(task["data"]["image"]).split('-')[1]
+            image_name = os.path.basename(task["data"]["image"])
 
-            # Create mask
-            mask_array = self.__convert_polygons_to_mask(polygons=polygons,
-                                                         image_width=image_width,
-                                                         image_height=image_height)
+            # Create annotation
+            mask_array = self.__convert_polygons_to_annotation(polygons=polygons,
+                                                               image_width=image_width,
+                                                               image_height=image_height)
 
-            # Save mask as images
-            self.__save_mask(mask_array=mask_array,
-                             image_name=image_name)
+            # # Save mask as images
+            # self.__save_mask(mask_array=mask_array,
+            #                  image_name=image_name)
+
+            break
