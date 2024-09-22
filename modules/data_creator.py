14cc1181ffcd3ee139b45747c917b7a3461b41b1
seblful <alesha.chimba@gmail.com>
2024-09-22 13:17:02 +0300

    [EDIT] get random image from pdf

--- modules/data_creator.py
+++ modules/data_creator.py
@@ -61,9 +61,9 @@ class DataCreator:
         pdf_path = os.path.join(raw_dir, pdf_name)
         pdf.save(pdf_path, version=17)
 
-    def get_page_image(self,
-                       page: pdfium.PdfPage,
-                       scale: int = 3) -> Image.Image:
+    def __get_page_image(self,
+                         page: pdfium.PdfPage,
+                         scale: int = 3) -> Image.Image:
         # Get image from pdf
         bitmap = page.render(scale=scale,
                              rotation=0)
@@ -76,6 +76,31 @@ class DataCreator:
             image = image.convert('RGB')
 
         return image
+
+    def __get_random_image_from_pdf(self,
+                                    pdf_listdir: list[str],
+                                    raw_dir: str,
+                                    train_dir: str) -> Tuple[Image.Image | str]:
+        # Take random pdf
+        rand_pdf_name = random.choice(pdf_listdir)
+        rand_pdf_path = os.path.join(raw_dir, rand_pdf_name)
+        rand_pdf_obj = pdfium.PdfDocument(rand_pdf_path)
+
+        # Take random pdf page and image
+        rand_page_ind = random.randint(0, len(rand_pdf_obj) - 1)
+        rand_page = rand_pdf_obj[rand_page_ind]
+
+        # Get random image and preprocess it
+        rand_image = self.__get_page_image(page=rand_page)
+
+        save_pdf_name = os.path.splitext(rand_pdf_name)[0]
+        rand_image_name = f"{save_pdf_name}_{rand_page_ind}.jpg"
+        rand_image_path = os.path.join(train_dir, rand_image_name)
+
+        # Close pdf file-object
+        rand_pdf_obj.close()
+
+        return rand_image, rand_image_path
 
     def create_yolo_train_data(self,
                                raw_dir: str,
@@ -92,23 +117,12 @@ class DataCreator:
 
         while num_images != num_saved_images:
 
-            # Take random pdf
-            rand_pdf_name = random.choice(pdf_listdir)
-            rand_pdf_path = os.path.join(raw_dir, rand_pdf_name)
-            rand_pdf_obj = pdfium.PdfDocument(rand_pdf_path)
-
-            # Take random pdf page and image
-            rand_page_ind = random.randint(0, len(rand_pdf_obj) - 1)
-            rand_page = rand_pdf_obj[rand_page_ind]
-
-            # Get random image and preprocess it
-            rand_image = self.get_page_image(page=rand_page)
+            rand_image, rand_image_path = self.__get_random_image_from_pdf(pdf_listdir=pdf_listdir,
+                                                                           raw_dir=raw_dir,
+                                                                           train_dir=train_dir)
+            # Process image
             rand_image = self.image_processor.process(image=rand_image,
                                                       scan_type=scan_type)
-
-            save_pdf_name = os.path.splitext(rand_pdf_name)[0]
-            rand_image_name = f"{save_pdf_name}_{rand_page_ind}.jpg"
-            rand_image_path = os.path.join(train_dir, rand_image_name)
 
             if not os.path.exists(rand_image_path):
                 rand_image.save(rand_image_path, "JPEG")
@@ -116,7 +130,6 @@ class DataCreator:
                 print(f"It was saved {num_saved_images}/{num_images} images.")
 
             rand_image.close()
-            rand_pdf_obj.close()
 
     @staticmethod
     def __read_classes_file(classes_path) -> Dict[int, str]:
@@ -208,10 +221,10 @@ class DataCreator:
 
         return Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 
-    def create_mask2f_train_data(self,
-                                 yolo_raw_dir: str,
-                                 train_dir: str,
-                                 num_images: int) -> None:
+    def create_mask2f_train_data_raw(self,
+                                     yolo_raw_dir: str,
+                                     train_dir: str,
+                                     num_images: int) -> None:
         # Paths
         images_dir = os.path.join(yolo_raw_dir, "images")
         labels_dir = os.path.join(yolo_raw_dir, "labels")
@@ -257,3 +270,19 @@ class DataCreator:
                 rand_image.save(save_image_path, "JPEG")
                 num_saved_images += 1
                 print(f"It was saved {num_saved_images}/{num_images} images.")
+
+    def create_mask2f_train_data_pred(self,
+                                      raw_dir: str,
+                                      train_dir: str,
+                                      yolo_model_path: str,
+                                      num_images: int) -> None:
+
+        # Pdf listdir
+        pdf_listdir = [pdf for pdf in os.listdir(
+            raw_dir) if pdf.endswith('pdf')]
+
+        # Counter for saved images
+        num_saved_images = 0
+
+        while num_images != num_saved_images:
+            pass
