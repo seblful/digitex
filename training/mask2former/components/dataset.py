a08820db9b77108e5f98934540d69a28e9de90e8
seblful <alesha.chimba@gmail.com>
2024-10-01 21:42:47 +0300

    [EDIT] procesor from pretrained

--- training/mask2former/components/dataset.py
+++ training/mask2former/components/dataset.py
@@ -2,7 +2,7 @@ import os
 
 from PIL import Image
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 from transformers import Mask2FormerImageProcessor
 from transformers.image_processing_base import BatchFeature
@@ -11,8 +11,9 @@ from transformers.image_processing_base import BatchFeature
 class Mask2FormerDataset(Dataset):
     def __init__(self,
                  set_dir: str,
-                 image_height: int,
-                 image_width: int) -> None:
+                 pretrained_model_name: str = None,
+                 image_height: int = 384,
+                 image_width: int = 384) -> None:
         # Dirs, paths with images, masks and classes
         self.set_dir = set_dir
         self.images_dir = os.path.join(set_dir, 'images')
@@ -25,10 +26,12 @@ class Mask2FormerDataset(Dataset):
 
         # Assert if number of images and annotation is the same
         assert len(self.images_listdir) == len(
-            self.masks_listdir), "Number of images must be equal number of annotations."
+            self.annotation_listdir), "Number of images must be equal number of annotations."
 
         # Feauture extractor
-        self.processor = Mask2FormerImageProcessor(do_reduce_labels=False)
+        self.processor = Mask2FormerImageProcessor.from_pretrained(pretrained_model_name,
+                                                                   do_resize=True,
+                                                                   do_reduce_labels=False)
         self.processor.size = {"height": image_height, "width": image_width}
 
     def __len__(self) -> int:
@@ -42,6 +45,8 @@ class Mask2FormerDataset(Dataset):
         # Open annotation
         annotation = Image.open(
             os.path.join(self.annotation_dir, self.annotation_listdir[idx]))
+
+        # TODO add image augmentation for train set
 
         # Create encoded inputs
         inputs = self.processor(
