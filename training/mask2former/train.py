a556b3f2034b637ba443fa3cd4a8d9eea07a7e7f
seblful <alesha.chimba@gmail.com>
2024-10-15 21:52:09 +0300

    [EDIT] new model, add args

--- training/mask2former/train.py
+++ training/mask2former/train.py
@@ -7,13 +7,22 @@ HOME = os.getcwd()
 DATASET_DIR = os.path.join(HOME, "data", "dataset")
 RUNS_DIR = os.path.join(HOME, "runs")
 
-PRETRAINED_MODEL = "facebook/mask2former-swin-base-coco-panoptic"
+PRETRAINED_MODEL = "facebook/mask2former-swin-tiny-coco-panoptic"
 
 
 def main() -> None:
     trainer = Mask2FormerTrainer(dataset_dir=DATASET_DIR,
                                  runs_dir=RUNS_DIR,
-                                 pretrained_model_name=PRETRAINED_MODEL)
+                                 pretrained_model_name=PRETRAINED_MODEL,
+                                 image_height=640,
+                                 image_width=640,
+                                 batch_size=2,
+                                 learning_rate=0.0001,
+                                 lr_scheduler_type="cosine",
+                                 mixed_precision="no",
+                                 train_epochs=100,
+                                 checkpoint_steps=500,
+                                 seed=2)
     trainer.train()
 
 
