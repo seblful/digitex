1b15a2960dbc54b9e5ff879ebd31224b7c5a3a4e
seblful <alesha.chimba@gmail.com>
2024-10-17 21:21:43 +0300

    [EDIT] model type, pretrained, patience

--- training/yolo/trainer.py
+++ training/yolo/trainer.py
@@ -1,78 +1,86 @@
 import os
 
 import torch
-import ultralytics
+from ultralytics import YOLO
 
 
 class Trainer():
     def __init__(self,
-                 dataset_dir,
-                 num_epochs,
-                 image_size,
-                 batch_size,
-                 seed,
-                 model_type='n'):
+                 dataset_dir: str,
+                 model_type: str,
+                 num_epochs: int,
+                 image_size: int,
+                 batch_size: int,
+                 pretrained_model_path: str = None,
+                 patience: int = 50,
+                 seed: int = 42) -> None:
 
+        # Data
         self.dataset_dir = dataset_dir
+        self.__data = None
+
+        # Parameters
         self.num_epochs = num_epochs
         self.image_size = image_size
         self.batch_size = batch_size
-        self.model_type = model_type
-
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+        self.patience = patience
         self.seed = seed
 
-        self.model_yaml = f"yolov8{model_type}-seg.yaml"
-        self.model_type = f"yolov8{model_type}-seg.pt"
-        self.__data = None
+        # Device
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print(f"Current device is {self.device}.")
+        self.device_count = torch.cuda.device_count()
+        self.device_idxs = [i for i in range(self.device_count)]
+
+        # Model
+        self.model_type = model_type
+        self.pretrained_model_path = pretrained_model_path
+        self.model_yaml = f"yolo11{model_type}-seg.yaml"
+        self.model_type = f"yolo11{model_type}-seg.pt"
 
         self.__model = None
         self.is_trained = False
 
     @property
-    def model(self):
+    def model(self) -> YOLO:
         if self.__model == None:
-            # Build a new model from scratch
-            model = ultralytics.YOLO(self.model_yaml)
-            # Load a pretrained model
-            model = ultralytics.YOLO(self.model_type)
-
-            # # Load from my pretrained model
-            # model = ultralytics.YOLO('best3.pt')
+            if self.pretrained_model_path is None:
+                # Build a new model from scratch and transfer COCO weights
+                model = YOLO(self.model_yaml).load(self.model_type)
+            else:
+                # Load from my pretrained model
+                model = YOLO(self.pretrained_model_path)
 
             self.__model = model
 
         return self.__model
 
     @property
-    def data(self):
+    def data(self) -> str:
         if self.__data is None:
             self.__data = os.path.join(self.dataset_dir, "data.yaml")
 
         return self.__data
 
-    def train(self):
-        '''
-        Training model
-        '''
+    def train(self) -> None:
+        self.model.train(data=self.data,
+                         epochs=self.num_epochs,
+                         imgsz=self.image_size,
+                         batch=self.batch_size,
+                         device=self.device_idxs,
+                         patience=self.patience,
+                         seed=self.seed)
 
-        result = self.model.train(data=self.data, epochs=self.num_epochs, imgsz=self.image_size,
-                                  batch=self.batch_size, seed=self.seed)  # close_mosaic=0, workers=1
         self.is_trained = True
 
-        return result
+        return None
 
-    def validate(self):
-        '''
-        Validating model on test dataset
-        '''
+    def validate(self) -> None:
+        if self.is_trained is False:
+            raise ValueError("Model must be trained before validating.")
 
-        if self.is_trained == True:
-            metrics = self.model.val(
-                data=self.data, imgsz=self.image_size, split='test')
+        self.model.val(data=self.data,
+                       imgsz=self.image_size,
+                       split='test')
 
-            return metrics
-
-        else:
-            return f"Model is not trained yet."
+        return
