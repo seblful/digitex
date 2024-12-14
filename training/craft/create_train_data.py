97dee2706776ab6ce654f6901752a6fcc5c31e77
seblful <alesha.chimba@gmail.com>
2024-12-14 14:19:54 +0300

    [EDIT] predict_parts

--- training/craft/create_train_data.py
+++ training/craft/create_train_data.py
@@ -6,6 +6,8 @@ from modules.data_creator import DataCreator
 HOME = os.getcwd()
 TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
 
+RAW_DATA_DIR = os.path.join(TESTING_DIR, "raw-data", "biology", "new")
+
 PARTS_TRAIN_DIR = os.path.join(HOME, "data", "train-data")
 
 YOLO_DIR = os.path.join(TESTING_DIR, "training", "yolo")
@@ -23,6 +25,14 @@ def main() -> None:
     # Create parts data from question annotations
     data_creator.extract_parts(question_raw_dir=QUESTION_RAW_DIR,
                                train_dir=PARTS_TRAIN_DIR,
+                               num_images=100)
+
+    # Create parts data from page and questions predictions
+    data_creator.predict_parts(raw_dir=RAW_DATA_DIR,
+                               train_dir=PARTS_TRAIN_DIR,
+                               yolo_page_model_path=YOLO_PAGE_PATH,
+                               yolo_question_model_path=YOLO_QUESTION_PATH,
+                               scan_type="color",
                                num_images=100)
 
 
