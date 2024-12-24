8af5910aaa707609974bde32b4c43128864117f5
seblful <alesha.chimba@gmail.com>
2024-12-24 22:57:43 +0300

    [EDIT] get_bbox_preds

--- modules/anns_converter.py
+++ modules/anns_converter.py
@@ -14,20 +14,45 @@ class AnnsConverter:
         return json_dict
 
     @staticmethod
-    def write_json(json_dict: dict,
+    def write_json(json_dicts: dict,
                    json_path: str,
                    indent: int = 4) -> None:
         with open(json_path, 'w', encoding="utf-8") as json_file:
-            json.dump(json_dict, json_file,
+            json.dump(json_dicts, json_file,
                       indent=indent,
                       ensure_ascii=False)
 
         return None
 
+    def get_bbox_preds(self, task: dict):
+        # Create empty list to results
+        preds = [{"result": []}]
+
+        result = task['annotations'][0]['result']
+
+        for entry in result:
+            if entry['type'] == 'textarea':
+                entry_copy = entry.copy()
+
+                del entry_copy['value']['text']
+
+                entry_copy['value']['rectanglelabels'] = ['text']
+
+                entry_copy['from_name'] = 'label'
+                entry_copy['to_name'] = 'image'
+                entry_copy['type'] = 'rectanglelabels'
+
+                preds[0]["result"].append(entry_copy)
+
+        return preds
+
     def ocr_to_bbox(self,
-                    json_path: str):
+                    ocr_json_path: str,
+                    output_dir: str) -> list[dict]:
         # Read json_path
-        json_dict = self.read_json(json_path)
+        ocr_json_dicts = self.read_json(ocr_json_path)
+
+        bbox_json_dicts = []
 
     def ocr_to_caption(self):
         pass
