f464adddd6247b45502ff7bd4eb6d6e0720a2af0
seblful <alesha.chimba@gmail.com>
2025-01-19 13:57:51 +0300

    [ADD] script to create and visualize dataset

---
+++ training/svtr2/create_dataset.py
@@ -0,0 +1,64 @@
+import os
+import argparse
+
+from components.data import DatasetCreator
+from components.visualizer import Visualizer
+
+
+# Create a parser
+parser = argparse.ArgumentParser(description="Get some hyperparameters.")
+
+# Get an arg for raw data source
+parser.add_argument("--source",
+                    default="synth",
+                    type=str,
+                    choices=["ls", "synth"],
+                    help="Raw data source.")
+
+# Get an arg for train split
+parser.add_argument("--train_split",
+                    default=0.9,
+                    type=float,
+                    help="Split of training dataset.")
+
+# Get an arg for word length
+parser.add_argument("--max_text_length",
+                    default=31,
+                    type=int,
+                    help="Max length of word in dataset.")
+
+# Get an arg for number of images to visualize
+parser.add_argument("--num_check_images",
+                    default=100,
+                    type=int,
+                    help="Number of images to visualize.")
+
+
+# Get our arguments from the parser
+args = parser.parse_args()
+
+
+HOME = os.getcwd()
+DATA = os.path.join(HOME, "data")
+SUBFOLDER = "finetune" if args.source == "ls" else "train"
+RAW_DIR = os.path.join(DATA, SUBFOLDER, "raw-data")
+DATASET_DIR = os.path.join(DATA, SUBFOLDER, "dataset")
+CHECK_IMAGES_DIR = os.path.join(DATA, SUBFOLDER, "check-images")
+
+
+def main() -> None:
+    # Initializing dataset creator and process data
+    dataset_creator = DatasetCreator(raw_dir=RAW_DIR,
+                                     dataset_dir=DATASET_DIR,
+                                     train_split=args.train_split,
+                                     max_text_length=args.max_text_length)
+    dataset_creator.create_dataset(source=args.source)
+
+    # Visualize dataset annotations
+    visualizer = Visualizer(dataset_dir=DATASET_DIR,
+                            check_images_dir=CHECK_IMAGES_DIR)
+    visualizer.visualize(num_images=args.num_check_images)
+
+
+if __name__ == '__main__':
+    main()
