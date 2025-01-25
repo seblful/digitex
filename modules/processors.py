fd2581dbf474fff8d7f01fac64f5dda4b169caef
seblful <alesha.chimba@gmail.com>
2025-01-25 16:32:07 +0300

    [EDIT] FileProcessor

--- modules/processors.py
+++ modules/processors.py
@@ -1,3 +1,6 @@
+import json
+import yaml
+
 from PIL import Image
 
 import numpy as np
@@ -96,7 +99,8 @@ class ImageProcessor:
                 illuminate: bool = False,
                 binarize: bool = False) -> Image.Image:
         # Check scan type
-        assert scan_type in self.scan_types, f"Scan type should be in one of {str(self.scan_types)}"
+        assert scan_type in self.scan_types, f"Scan type should be in one of {
+            str(self.scan_types)}"
 
         # Convert image to array
         img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
@@ -117,3 +121,49 @@ class ImageProcessor:
             img = self.binarize_image(img)
 
         return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+
+class FileProcessor:
+    @staticmethod
+    def read_txt(txt_path) -> list[str]:
+        with open(txt_path, "r", encoding="utf-8") as txt_file:
+            content = txt_file.readlines()
+
+        return content
+
+    @staticmethod
+    def write_txt(txt_path: str,
+                  lines: list[str]) -> None:
+        with open(txt_path, 'w', encoding="utf-8") as txt_file:
+            txt_file.writelines(lines)
+
+        return None
+
+    @staticmethod
+    def read_json(json_path) -> dict:
+        with open(json_path, "r", encoding="utf-8") as json_file:
+            json_dict = json.load(json_file)
+
+        return json_dict
+
+    @staticmethod
+    def write_json(json_dict: dict,
+                   json_path: str,
+                   indent: int = 4) -> None:
+        with open(json_path, 'w', encoding="utf-8") as json_file:
+            json.dump(json_dict, json_file,
+                      indent=indent,
+                      ensure_ascii=False)
+
+        return None
+
+    @staticmethod
+    def write_yaml(yaml_path: str, data: dict, comment: str = None) -> None:
+        with open(yaml_path, 'w', encoding="utf-8") as yaml_file:
+            if comment:
+                yaml_file.write(comment)
+            yaml.dump(data, yaml_file,
+                      default_flow_style=False,
+                      allow_unicode=True)
+
+        return None
