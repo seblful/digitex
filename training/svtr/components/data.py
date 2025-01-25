09036eaddee1c51fa90729e135addbccb420dd1f
seblful <alesha.chimba@gmail.com>
2025-01-25 16:31:40 +0300

    [EDIT] create_gt

--- training/svtr/components/data.py
+++ training/svtr/components/data.py
@@ -2,12 +2,11 @@ import os
 import shutil
 import random
 
-import json
-import yaml
-
 from urllib.parse import unquote
 
 from tqdm import tqdm
+
+from modules.processors import FileProcessor
 
 
 class DatasetCreator():
@@ -15,10 +14,10 @@ class DatasetCreator():
                  raw_dir: str,
                  dataset_dir: str,
                  train_split: float = 0.8,
-                 max_text_length=25) -> None:
+                 max_text_length=31) -> None:
+
         # Input paths
         self.raw_dir = raw_dir
-        self.raw_images_dir = os.path.join(raw_dir, "images")
         self.chars_txt_path = os.path.join(raw_dir, "chars.txt")
         self.replaces_json_path = os.path.join(
             raw_dir, "replaces.json")
@@ -26,8 +25,6 @@ class DatasetCreator():
         # Output paths
         self.dataset_dir = dataset_dir
         self.charset_txt_path = os.path.join(dataset_dir, "charset.txt")
-        self.gt_json_path = os.path.join(raw_dir, "gt.json")
-        
 
         # Data split
         self.__setup_splits(train_split)
@@ -36,11 +33,8 @@ class DatasetCreator():
         self.sources = ["ls", "synth"]
 
         # Annotation creator
-        self.annotation_creator = AnnotationCreator(raw_images_dir=self.raw_images_dir,
-                                                    chars_txt_path=self.chars_txt_path,
+        self.annotation_creator = AnnotationCreator(charset=self.charset,
                                                     replaces_json_path=self.replaces_json_path,
-                                                    charset_txt_path = self.charset_txt_path,
-                                                    gt_json_path=self.gt_json_path,
                                                     max_text_length=max_text_length)
 
     def __setup_splits(self, train_split: float) -> None:
@@ -60,6 +54,14 @@ class DatasetCreator():
             image_dir = os.path.join(dir, "images")
             os.makedirs(image_dir)
 
+    @property
+    def charset(self) -> set[str]:
+        if self.__charset is None:
+            charset = FileProcessor.read_txt(self.chars_txt_path)[0].strip()
+            self.__charset = set(charset)
+
+        return self.__charset
+
     @staticmethod
     def sort_gt_key(key) -> int:
         filepath = key[0]
@@ -77,7 +79,7 @@ class DatasetCreator():
                     gt: dict[str, str],
                     set_dir: str) -> None:
         dir_name = os.path.basename(set_dir)
-        
+
         # Create list to store gt lines for txt gt
         gt_lines = []
 
@@ -97,6 +99,20 @@ class DatasetCreator():
         gt_txt_path = os.path.join(set_dir, "gt.txt")
         self.annotation_creator.write_txt(txt_path=gt_txt_path,
                                           lines=gt_lines)
+
+    def __create_charset(self) -> None:
+        chars = sorted(self.charset)
+
+        # Create lines with char and new line
+        charlines = []
+        for char in chars:
+            charline = char + "\n"
+            charlines.append(charline)
+
+        # Write lines to txt
+        FileProcessor.write_txt(self.charset_txt_path, charlines)
+
+        return None
 
     def __partitionate_data(self) -> None:
         # Load gt dict and shuffle
@@ -131,112 +147,43 @@ class DatasetCreator():
         # Create annotations
         if source == "ls":
             data_json_path = os.path.join(self.raw_dir, 'data.json')
-            self.annotation_creator.create_annotations_from_ls(
+            self.annotation_creator.create_ls_gt(
                 data_json_path=data_json_path)
         else:
             gt_txt_path = os.path.join(self.raw_dir, "gt.txt")
-            self.annotation_creator.create_annotations_from_synth(
+            self.annotation_creator.create_synth_gt(
                 gt_txt_path=gt_txt_path)
 
         # Create dataset
         self.__partitionate_data()
 
+        # Create charser
+        self.__create_charset()
+
 
 class AnnotationCreator:
     def __init__(self,
-                 raw_images_dir: str,
-                 chars_txt_path: str,
+                 charset: set[str],
                  replaces_json_path: str,
-                 charset_txt_path:str,
-                 gt_json_path: str,
                  max_text_length: int) -> None:
+        self.charset = charset
+
         # Paths
-        self.raw_images_dir = raw_images_dir
-        self.chars_txt_path = chars_txt_path
         self.replaces_json_path = replaces_json_path
-        
-        self.charset_txt_path = charset_txt_path
-        self.gt_json_path = gt_json_path
 
         self.max_text_length = max_text_length
 
-        self.__charset = None
         self.__replaces_table = None
-
-    @property
-    def charset(self) -> set[str]:
-        if self.__charset is None:
-            charset = self.read_txt(self.chars_txt_path)[0].strip()
-            self.__charset = set(charset)
-
-        return self.__charset
 
     @property
     def replaces_table(self) -> dict:
         if self.__replaces_table is None:
-            replaces_dict = self.read_json(self.replaces_json_path)
+            replaces_dict = FileProcessor.read_json(self.replaces_json_path)
             self.__replaces_table = str.maketrans(replaces_dict)
 
         return self.__replaces_table
 
-    @staticmethod
-    def read_txt(txt_path) -> list[str]:
-        with open(txt_path, "r", encoding="utf-8") as txt_file:
-            content = txt_file.readlines()
-
-        return content
-
-    @staticmethod
-    def write_txt(txt_path: str,
-                  lines: list[str]) -> None:
-        with open(txt_path, 'w', encoding="utf-8") as txt_file:
-            txt_file.writelines(lines)
-
-        return None
-
-    @staticmethod
-    def read_json(json_path) -> dict:
-        with open(json_path, "r", encoding="utf-8") as json_file:
-            json_dict = json.load(json_file)
-
-        return json_dict
-
-    @staticmethod
-    def write_json(json_dict: dict,
-                   json_path: str,
-                   indent: int = 4) -> None:
-        with open(json_path, 'w', encoding="utf-8") as json_file:
-            json.dump(json_dict, json_file,
-                      indent=indent,
-                      ensure_ascii=False)
-
-        return None
-
-    @staticmethod
-    def write_yaml(yaml_path: str, data: dict, comment: str = None) -> None:
-        with open(yaml_path, 'w', encoding="utf-8") as yaml_file:
-            if comment:
-                yaml_file.write(comment)
-            yaml.dump(data, yaml_file,
-                      default_flow_style=False,
-                      allow_unicode=True)
-
-        return None
-
-    def __get_text(self, task: dict) -> str:
-        # Retrieve result and label
-        result = task['annotations'][0]['result']
-        text = result[0]['value']['text'][0]
-
-        return text
-
-    def __get_image_path(self, task: dict) -> str:
-        image_path = unquote(os.path.basename(task["data"]["captioning"]))
-        # image_path = os.path.join(self.raw_images_dir, image_name)
-
-        return image_path
-
-    def check_length(self, text: str):
+    def check_length(self, text: str) -> None:
         if len(text) > self.max_text_length:
             raise ValueError(f"Length of text {text} is greater than {
                              self.max_text_length}.")
@@ -256,34 +203,33 @@ class AnnotationCreator:
         for image_path, text in zip(image_paths, texts):
             gt[image_path] = text
 
-        # Write gts to json
-        self.write_json(gt, self.gt_json_path)
-        
-    def create_charset(self) -> None:
-        chars = sorted(self.charset)
-        
-        # Create lines with char and new line
-        charlines = []
-        for char in chars:
-            charline = char + "\n"
-            charlines.append(charline)
-        
-        # Write lines to txt    
-        self.write_txt(self.charset_txt_path, charlines)
-        
-        return None
+        return gt
 
-    def create_annotations_from_ls(self,
-                                   data_json_path: str) -> None:
+    def __get_text(self, task: dict) -> str:
+        # Retrieve result and label
+        result = task['annotations'][0]['result']
+        text = result[0]['value']['text'][0]
+
+        return text
+
+    def __get_image_path(self, task: dict, images_dir: str) -> str:
+        image_path = unquote(os.path.basename(task["data"]["captioning"]))
+        image_path = os.path.join(images_dir, image_path)
+
+        return image_path
+
+    def create_ls_gt(self,
+                     images_dir: str,
+                     data_json_path: str) -> dict[str, str]:
         # Read jsons
-        json_dict = self.read_json(data_json_path)
+        json_dict = FileProcessor.read_json(data_json_path)
 
         texts = []
         image_paths = []
 
         # Iterate through task
-        for task in tqdm(json_dict, desc="Creating annotations"):
-            image_path = self.__get_image_path(task)
+        for task in tqdm(json_dict, desc="Creating annotations from Label Studio"):
+            image_path = self.__get_image_path(task, images_dir)
             text = self.__get_text(task)
 
             # Replace chars that not in the charset
@@ -297,22 +243,23 @@ class AnnotationCreator:
             image_paths.append(image_path)
 
         # Create GT labels
-        self.create_gt(image_paths, texts)
-        
-        # Create charset
-        self.create_charset()
+        gt = self.create_gt(image_paths, texts)
 
-    def create_annotations_from_synth(self,
-                                      gt_txt_path: str) -> None:
-        lines = self.read_txt(gt_txt_path)
+        return gt
+
+    def create_synth_gt(self,
+                        images_dir: str,
+                        gt_txt_path: str) -> dict[str, str]:
+        lines = FileProcessor.read_txt(gt_txt_path)
 
         texts = []
         image_paths = []
 
-        for line in tqdm(lines, desc="Creating annotations"):
+        for line in tqdm(lines, desc="Creating annotations from synthesized data"):
             line = line.strip()
             image_path, text = line.split(maxsplit=1)
             image_path = os.path.relpath(image_path, start="images")
+            image_path = os.path.join(images_dir, image_path)
 
             # Replace chars that not in the charset
             text = text.translate(self.replaces_table)
@@ -325,7 +272,6 @@ class AnnotationCreator:
             image_paths.append(image_path)
 
         # Create GT labels
-        self.create_gt(image_paths, texts)
-        
-        # Create charset
-        self.create_charset()
+        gt = self.create_gt(image_paths, texts)
+
+        return gt
