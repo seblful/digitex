d3fd6e47638b63b7e08636cf628c464871b1003c
seblful <alesha.chimba@gmail.com>
2025-02-09 15:45:02 +0300

    [EDIT] create table train data

--- training/yolo/create_train_data.py
+++ training/yolo/create_train_data.py
@@ -9,11 +9,16 @@ HOME = os.getcwd()
 TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
 IMAGES_DIR = os.path.join(TESTING_DIR, "raw-data", "biology", "images")
 RAW_DATA_DIR = os.path.join(TESTING_DIR, "raw-data", "biology", "new")
-TRAIN_PAGE_DIR = os.path.join(HOME, "data", "page", "train-data")
-TRAIN_QUESTION_DIR = os.path.join(HOME, "data", "question", "train-data")
 
 PAGE_RAW_DIR = os.path.join(HOME, "data", "page", "raw-data")
-YOLO_PAGE_PATH = os.path.join(HOME, "models", "yolov11", "page_m.pt")
+PAGE_TRAIN_DIR = os.path.join(HOME, "data", "page", "train-data")
+PAGE_YOLO_PATH = os.path.join(HOME, "models", "page", "page_m.pt")
+
+QUESTION_RAW_DIR = os.path.join(HOME, "data", "question", "raw-data")
+QUESTION_TRAIN_DIR = os.path.join(HOME, "data", "question", "train-data")
+QUESTION_YOLO_PATH = os.path.join(HOME, "models", "question", "question_x3.pt")
+
+TABLE_TRAIN_DIR = os.path.join(HOME, "data", "table", "train-data")
 
 
 def main() -> None:
@@ -28,21 +33,36 @@ def main() -> None:
 
     # # Create data for page
     # data_creator.extract_pages(raw_dir=RAW_DATA_DIR,
-    #                            train_dir=TRAIN_PAGE_DIR,
+    #                            train_dir=PAGE_TRAIN_DIR,
     #                            scan_type="color",
     #                            num_images=100)
 
     # # Create data for question from annotations
     # data_creator.extract_questions(page_raw_dir=PAGE_RAW_DIR,
-    #                                train_dir=TRAIN_QUESTION_DIR,
+    #                                train_dir=QUESTION_TRAIN_DIR,
     #                                num_images=100)
 
-    # Create data for question from YOLO predictions
-    data_creator.predict_questions(raw_dir=RAW_DATA_DIR,
-                                   train_dir=TRAIN_QUESTION_DIR,
-                                   yolo_model_path=YOLO_PAGE_PATH,
-                                   scan_type="color",
-                                   num_images=100)
+    # # Create data for question from YOLO predictions
+    # data_creator.predict_questions(raw_dir=RAW_DATA_DIR,
+    #                                train_dir=QUESTION_TRAIN_DIR,
+    #                                yolo_model_path=PAGE_YOLO_PATH,
+    #                                scan_type="color",
+    #                                num_images=100)
+
+    # Create data for table from annotations
+    data_creator.extract_parts(question_raw_dir=QUESTION_RAW_DIR,
+                               train_dir=TABLE_TRAIN_DIR,
+                               num_images=80,
+                               target_classes=["table"])
+
+    # Create data for table from YOLO predictions
+    data_creator.predict_parts(raw_dir=RAW_DATA_DIR,
+                               train_dir=TABLE_TRAIN_DIR,
+                               yolo_page_model_path=PAGE_YOLO_PATH,
+                               yolo_question_model_path=QUESTION_YOLO_PATH,
+                               scan_type="color",
+                               num_images=80,
+                               target_classes=["table"])
 
 
 if __name__ == "__main__":
