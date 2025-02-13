830077bd428ea4d8085bce6083d2359301b04414
seblful <alesha.chimba@gmail.com>
2025-02-13 23:07:44 +0300

    [EDIT] to numpy array

--- training/yolo/components/converter.py
+++ training/yolo/components/converter.py
@@ -7,11 +7,9 @@ class Converter:
     def xyxyxyxy_to_polygon(xyxyxyxy: list[float],
                             img_width: int,
                             img_height: int) -> np.ndarray:
-        # TODO np.array, reshape, w and h
-        xyxyxyxy = [xyxyxyxy[i] * (img_width if i % 2 == 0 else img_height)
-                    for i in range(len(xyxyxyxy))]
         xyxyxyxy = np.array(xyxyxyxy)
         polygon = xyxyxyxy.reshape((-1, 2))
+        polygon = polygon * np.array((img_width, img_height))
 
         return polygon
 
@@ -19,8 +17,8 @@ class Converter:
     def point_to_polygon(point: list[float],
                          img_width: int,
                          img_height: int) -> np.ndarray:
-        # TODO np.array and then reshape((-1, 2))
-        polygon = list(zip(point[::2], point[1::2]))
+        point = np.array(point)
+        polygon = point.reshape((-1, 2))
         polygon = np.array(polygon) * np.array((img_width, img_height))
 
         return polygon
