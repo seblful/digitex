f6a0fa78611bf8ad2b27d2bbf20ec35b07902339
seblful <alesha.chimba@gmail.com>
2025-02-14 20:24:22 +0300

    [EDIT] get_random_image

--- training/yolo/components/utils.py
+++ training/yolo/components/utils.py
@@ -6,11 +6,18 @@ from PIL import Image
 import numpy as np
 
 
+def get_random_image(images_dir,
+                     images_listdir: list[str]) -> tuple[np.ndarray, str]:
+    image_name = random.choice(images_listdir)
+    image_path = os.path.join(images_dir, image_name)
+    image = Image.open(image_path)
+
+    return image_name, image
+
+
 def get_random_img(images_dir,
                    images_listdir: list[str]) -> tuple[np.ndarray, str]:
-    img_name = random.choice(images_listdir)
-    img_path = os.path.join(images_dir, img_name)
-    image = Image.open(img_path)
+    img_name, image = get_random_image(images_dir, images_listdir)
     img = np.array(image)
 
     return img_name, img
