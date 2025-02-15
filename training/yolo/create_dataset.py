2845430cf89e755e15e6d6e239961bc4f54803c2
seblful <alesha.chimba@gmail.com>
2025-02-15 14:12:23 +0300

    [EDIT] augment and visualize

--- training/yolo/create_dataset.py
+++ training/yolo/create_dataset.py
@@ -1,8 +1,6 @@
 import os
 import argparse
 
-import numpy as np
-
 from components.dataset import DatasetCreator
 from components.augmenter import Augmenter
 from components.visualizer import Visualizer
@@ -11,30 +9,33 @@ from components.visualizer import Visualizer
 # Create a parser
 parser = argparse.ArgumentParser(description="Get some hyperparameters.")
 
-# Get an arg for task type
 parser.add_argument("--data_subdir",
                     default="page",
                     type=str,
                     help="Type of task type.")
 
-# Get an arg for train split
 parser.add_argument("--train_split",
                     default=0.8,
                     type=float,
-                    help="Split of training dataset.")
+                    help="Split of train set.")
 
 parser.add_argument("--augment",
                     action="store_true",
                     help="Whether to augment train data.")
 
-parser.add_argument("--aug_factor",
-                    default=3,
+parser.add_argument("--aug_images",
+                    default=100,
                     type=int,
                     help="How many augmented images to create.")
 
 parser.add_argument("--visualize",
                     action="store_true",
                     help="Whether to visualize data.")
+
+parser.add_argument("--vis_images",
+                    default=20,
+                    type=int,
+                    help="How many images to visualize.")
 
 
 # Get our arguments from the parser
@@ -44,8 +45,11 @@ args = parser.parse_args()
 DATA_SUBDIR = args.data_subdir
 TRAIN_SPLIT = args.train_split
 AUGMENT = args.augment
-AUG_FACTOR = args.aug_factor
+AUG_IMAGES = args.aug_images
 VISUALIZE = args.visualize
+VIS_IMAGES = args.vis_images
+
+ANNS_TYPE = "polygon" if DATA_SUBDIR in ["page", "question"] else "obb"
 
 HOME = os.getcwd()
 DATA_DIR = os.path.join(HOME, "data", DATA_SUBDIR)
@@ -53,12 +57,9 @@ RAW_DIR = os.path.join(DATA_DIR, 'raw-data')
 DATASET_DIR = os.path.join(DATA_DIR, 'dataset')
 CHECK_IMAGES_DIR = os.path.join(DATA_DIR, "check-images")
 
-# Overcome bool error related to new NumPy version
-np.bool = np.bool_
-
 
 def main() -> None:
-    # Initializing dataset creator and process data
+    # Create dataset
     dataset_creator = DatasetCreator(RAW_DIR,
                                      DATASET_DIR,
                                      train_split=TRAIN_SPLIT)
@@ -66,14 +67,16 @@ def main() -> None:
 
     # Augment dataset
     if AUGMENT:
-        augmenter = Augmenter(dataset_dir=DATASET_DIR,
-                              aug_factor=AUG_FACTOR)
-        augmenter.augment()
+        augmenter = Augmenter(dataset_dir=DATASET_DIR)
+        augmenter.augment(anns_type=ANNS_TYPE,
+                          num_images=AUG_IMAGES)
 
+    # Visualize dataset
     if VISUALIZE:
         visualizer = Visualizer(dataset_dir=DATASET_DIR,
                                 check_images_dir=CHECK_IMAGES_DIR)
-        visualizer.visualize()
+        visualizer.visualize(anns_type=ANNS_TYPE,
+                             num_images=VIS_IMAGES)
 
 
 if __name__ == '__main__':
