2f2c89114a0177d7a1f35258b3c02a8006aba907
seblful <alesha.chimba@gmail.com>
2025-03-30 13:12:03 +0300

    [EDIT] setup panes

--- extract/components/extractor.py
+++ extract/components/extractor.py
@@ -1,5 +1,10 @@
+import os
+from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
+
+from modules.handlers import PDFHandler
+from modules.processors import FileProcessor
 
 
 class ExtractorApp:
@@ -7,7 +12,20 @@ class ExtractorApp:
         self.root = root
         self.setup_root()
 
-        self.ui = UserInterface(self.root)
+        # Dirs
+        self.inputs_dir = "inputs"
+        self.ckpt_path = os.path.join(
+            self.inputs_dir, "checkpoints.json")
+
+        self.current_pdf_path = None
+        self.current_pdf_obj = None
+        self.current_image = None
+        self.current_page = 0
+
+        self.ui = UserInterface(self.root, self)
+
+        self.pdf_handler = PDFHandler()
+        self.file_processor = FileProcessor()
 
     def setup_root(self) -> None:
         self.root.title("Testing Digitizer")
@@ -18,17 +36,41 @@ class ExtractorApp:
 
 
 class UserInterface:
-    def __init__(self, root) -> None:
+    def __init__(self, root: tk.Tk, main_app: ExtractorApp) -> None:
         self.root = root
+        self.main_app = main_app
+
         self.setup_ui()
 
     def setup_ui(self) -> None:
         self.setup_menubar()
+        self.setup_panes()
+        self.setup_status_bar()
 
     def setup_menubar(self) -> None:
         menubar = tk.Menu(self.root)
         file_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="File", menu=file_menu)
-        # file_menu.add_command(label="Open", command=self.open_pdf)
+        file_menu.add_command(label="Open", command=self.main_app.open_pdf)
+        file_menu.add_command(label="Load checkpoints",
+                              command=self.main_app.load_ckpt)
         file_menu.add_command(label="Exit", command=self.root.quit)
         self.root.config(menu=menubar)
+
+    def setup_panes(self) -> None:
+        main_pane = ttk.PanedWindow(self.root, orient=tk.HORIZONTAL)
+        self.left_pane = ttk.PanedWindow(main_pane)
+        self.right_pane = ttk.PanedWindow(main_pane, orient=tk.VERTICAL)
+
+        self.setup_left_frame()
+
+    def setup_left_frame(self) -> None:
+        self.left_frame = ttk.Frame(self.left_pane)
+        self.left_canvas = tk.Canvas(self.left_frame, bg='white')
+        self.left_canvas.pack(expand=True, fill=tk.BOTH)
+        # self.setup_navigation_controls(self.left_pane)
+
+    def setup_status_bar(self) -> None:
+        self.main_app.status = ttk.Label(
+            self.root, text="Ready", relief=tk.SUNKEN)
+        self.main_app.status.pack(side=tk.BOTTOM, fill=tk.X)
