a0f972fb6c522bdc90b79edb39d9296b1d0b2d46
seblful <alesha.chimba@gmail.com>
2025-04-15 21:58:27 +0300

    [EDIT] refactor PageDataCreator: update extract method to improve image extraction logic

--- src/digitex/core/creators/page.py
+++ src/digitex/core/creators/page.py
@@ -3,21 +3,15 @@ from .base import BaseDataCreator
 
 
 class PageDataCreator(BaseDataCreator):
-    def extract_pages(
-        self, raw_dir: str, train_dir: str, scan_type: str, num_images: int
-    ) -> None:
-        pdf_listdir = [pdf for pdf in os.listdir(raw_dir) if pdf.endswith("pdf")]
+    def extract(self, pdf_dir: str, train_dir: str, num_images: int) -> None:
+        pdf_listdir = [pdf for pdf in os.listdir(pdf_dir) if pdf.endswith("pdf")]
         num_saved = 0
 
         while num_images != num_saved:
-            rand_image, rand_image_name, rand_page_idx = (
-                self.pdf_handler.get_random_image(
-                    pdf_listdir=pdf_listdir, pdf_dir=raw_dir
-                )
+            rand_image, rand_image_name, rand_page_idx = self._get_pdf_random_image(
+                pdf_listdir=pdf_listdir, pdf_dir=pdf_dir
             )
-            rand_image = self.img_processor.process(
-                image=rand_image, scan_type=scan_type
-            )
+            rand_image = self._process_image(image=rand_image)
             num_saved = self._save_image(
                 rand_page_idx,
                 output_dir=train_dir,
