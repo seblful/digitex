9bdd9ab59c1b864dba33a6f0f013582516710ff6
seblful <alesha.chimba@gmail.com>
2025-04-15 19:00:09 +0300

    [EDIT] minor changes

--- digitex/training/yolo/create_train_data.py
+++ digitex/training/yolo/create_train_data.py
@@ -5,10 +5,9 @@ from digitex.core.utils import create_pdf_from_images
 
 # Paths
 HOME = os.getcwd()
-
-TESTING_DIR = os.path.dirname(os.path.dirname(HOME))
-IMAGES_DIR = os.path.join(TESTING_DIR, "raw-data", "biology", "images")
-RAW_DATA_DIR = os.path.join(TESTING_DIR, "raw-data", "biology", "new")
+BOOKS_DIR = os.path.join(HOME, "books")
+IMAGES_DIR = os.path.join(BOOKS_DIR, "biology", "images")
+RAW_DATA_DIR = os.path.join(BOOKS_DIR, "biology", "new")
 
 PAGE_RAW_DIR = os.path.join(HOME, "data", "page", "raw-data")
 PAGE_TRAIN_DIR = os.path.join(HOME, "data", "page", "train-data")
@@ -22,15 +21,14 @@ TABLE_TRAIN_DIR = os.path.join(HOME, "data", "table", "train-data")
 
 
 def main() -> None:
-    # Create DataCreator instance
-    data_creator = PageDataCreator()
+    # Create pdfs from images
+    for image_dir in os.listdir(IMAGES_DIR):
+        image_dir = os.path.join(IMAGES_DIR, image_dir)
+        create_pdf_from_images(image_dir=image_dir, output_dir=RAW_DATA_DIR)
+        break
 
-    # # Creat pdfs from images
-    # for image_dir in os.listdir(IMAGES_DIR):
-    #     image_dir = os.path.join(IMAGES_DIR, image_dir)
-    #     create_pdf_from_images(image_dir=image_dir,
-    #                            raw_dir=RAW_DATA_DIR)
-
+    # # Create DataCreator instance
+    # data_creator = PageDataCreator()
     # # Create data for page
     # data_creator.extract_pages(raw_dir=RAW_DATA_DIR,
     #                            train_dir=PAGE_TRAIN_DIR,
@@ -49,24 +47,24 @@ def main() -> None:
     #                                scan_type="color",
     #                                num_images=100)
 
-    # Create data for table from annotations
-    data_creator.extract_parts(
-        question_raw_dir=QUESTION_RAW_DIR,
-        train_dir=TABLE_TRAIN_DIR,
-        num_images=80,
-        target_classes=["table"],
-    )
+    # # Create data for table from annotations
+    # data_creator.extract_parts(
+    #     question_raw_dir=QUESTION_RAW_DIR,
+    #     train_dir=TABLE_TRAIN_DIR,
+    #     num_images=80,
+    #     target_classes=["table"],
+    # )
 
-    # Create data for table from YOLO predictions
-    data_creator.predict_parts(
-        raw_dir=RAW_DATA_DIR,
-        train_dir=TABLE_TRAIN_DIR,
-        yolo_page_model_path=PAGE_YOLO_PATH,
-        yolo_question_model_path=QUESTION_YOLO_PATH,
-        scan_type="color",
-        num_images=80,
-        target_classes=["table"],
-    )
+    # # Create data for table from YOLO predictions
+    # data_creator.predict_parts(
+    #     raw_dir=RAW_DATA_DIR,
+    #     train_dir=TABLE_TRAIN_DIR,
+    #     yolo_page_model_path=PAGE_YOLO_PATH,
+    #     yolo_question_model_path=QUESTION_YOLO_PATH,
+    #     scan_type="color",
+    #     num_images=80,
+    #     target_classes=["table"],
+    # )
 
 
 if __name__ == "__main__":
