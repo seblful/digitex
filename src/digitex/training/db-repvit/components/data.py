323e319c61d592c86da39819c4dab3191d8dc3a2
seblful <alesha.chimba@gmail.com>
2025-04-21 22:53:20 +0300

    [EDIT] only bbox to points

--- src/digitex/training/db-repvit/components/data.py
+++ src/digitex/training/db-repvit/components/data.py
@@ -28,49 +28,33 @@ class AnnotationCreator:
         return hash
 
     @staticmethod
-    def bbox_to_polygon(
+    def bbox_to_points(
         bbox: dict, image_width: int, image_height: int
     ) -> list[tuple[int, int]]:
-        x_rel, y_rel, width_rel, height_rel, rotation = (
-            bbox["x"],
-            bbox["y"],
-            bbox["width"],
-            bbox["height"],
-            bbox["rotation"],
-        )
-
         # Convert relative coordinates to absolute
-        x = (x_rel / 100) * image_width
-        y = (y_rel / 100) * image_height
-        width = (width_rel / 100) * image_width
-        height = (height_rel / 100) * image_height
+        x = (bbox["x"] / 100) * image_width
+        y = (bbox["y"] / 100) * image_height
+        width = (bbox["width"] / 100) * image_width
+        height = (bbox["height"] / 100) * image_height
 
         # Define the vertices of the rectangle (unrotated)
         vertices = [(x, y), (x + width, y), (x + width, y + height), (x, y + height)]
 
         # Convert rotation to radians
-        angle_rad = math.radians(rotation)
+        angle_rad = math.radians(bbox["rotation"])
 
         # Define the rotation matrix
         cos_theta = math.cos(angle_rad)
         sin_theta = math.sin(angle_rad)
 
         # Rotate each vertex around the top-left corner of the bounding box
-        rotated_polygon = []
+        points = []
         for vx, vy in vertices:
             rx = x + (vx - x) * cos_theta - (vy - y) * sin_theta
             ry = y + (vx - x) * sin_theta + (vy - y) * cos_theta
-            rotated_polygon.append((int(rx), int(ry)))
+            points.append((int(rx), int(ry)))
 
-        return rotated_polygon
-
-    @staticmethod
-    def polygon_to_abs(polygon, image_width: int, image_height: int) -> None:
-        for i in range(len(polygon)):
-            polygon[i][0] = int((polygon[i][0] / 100) * image_width)
-            polygon[i][1] = int((polygon[i][1] / 100) * image_height)
-
-        return None
+        return points
 
     def _get_annotation(self, task) -> list[dict]:
         # Create empty array to store data
@@ -84,41 +68,23 @@ class AnnotationCreator:
             image_width = entry["original_width"]
             image_height = entry["original_height"]
 
-            # Create empty dict to store transcription and points
-            ann = {}
-            # Extract points (polygon)
-            value_keys = entry["value"].keys()
-
-            # Extract annotation from points
-            if {"points", "text"}.issubset(value_keys):
-                ann["transcription"] = entry["value"]["text"][0]
-                polygon = entry["value"]["points"]
-                self.polygon_to_abs(
-                    polygon=polygon, image_width=image_width, image_height=image_height
-                )
-                polygon = [tuple(points) for points in polygon]
-                ann["points"] = polygon
-
-                annotation.append(ann)
-
             # Extract annotation from bbox
-            elif {"x", "text"}.issubset(value_keys):
+            if {"x", "text"}.issubset(entry["value"].keys()):
+                ann = {}
                 ann["transcription"] = entry["value"]["text"][0]
-                bbox = {
-                    k: v for k, v in entry["value"].items() if k != "rectanglelabels"
-                }
+                bbox = {k: v for k, v in entry["value"].items() if k != "text"}
 
-                polygon = self.bbox_to_polygon(
+                points = self.bbox_to_points(
                     bbox=bbox, image_width=image_width, image_height=image_height
                 )
-                ann["points"] = polygon
+                ann["points"] = points
 
                 annotation.append(ann)
 
         return annotation
 
     def create_annotations(self) -> None:
-        # Read json_polygon_path
+        # Read json data
         json_dict = FileProcessor.read_json(self.data_json_path)
 
         # Create empty dict to store formatted annotations
@@ -126,7 +92,7 @@ class AnnotationCreator:
 
         # Iterating through tasks
         for task in json_dict:
-            # Get polygons and labels
+            # Get annotation
             ann = self._get_annotation(task)
 
             # Fill ann dict
