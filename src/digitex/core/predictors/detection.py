3d78f8352305ced4e4def12273c84912ecf569d3
seblful <alesha.chimba@gmail.com>
2025-04-27 12:22:25 +0300

    [EDIT] DP_RepVitDetectionPredictor

--- src/digitex/core/predictors/detection.py
+++ src/digitex/core/predictors/detection.py
@@ -1,71 +1,63 @@
 from PIL import Image
 
 import numpy as np
-import torch
 
-from doctr.models import detection_predictor, fast_base
+from openocr import OpenDetector
+from openocr.tools.engine.config import Config
 
 from .abstract_predictor import Predictor
 from .prediction_result import DetectionPredictionResult
 
 
-class FAST_DetectionPredictor(Predictor):
-    def __init__(self,
-                 model_path: str) -> None:
+class DP_RepVitDetectionPredictor(Predictor):
+    def __init__(self, model_path: str, config_path: str, device: str) -> None:
         self.model_path = model_path
-        self.device = torch.device(
-            "cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.config_path = config_path
+        self.device = device
+
+        self.__config = None
         self.__model = None
 
     @property
-    def model(self) -> detection_predictor:
-        if self.__model is None:
-            torch_model = fast_base(pretrained=False,
-                                    pretrained_backbone=False)
-            params = torch.load(self.model_path, map_location=self.device)
-            torch_model.load_state_dict(params)
+    def config(self) -> dict[str]:
+        if self.__config is None:
+            config = Config(self.config_path).cfg
+            config["Global"]["pretrained_model"] = self.model_path
+            config["Global"]["device"] = "gpu" if self.device == "cuda" else "cpu"
 
-            model = detection_predictor(torch_model,
-                                        pretrained=True,
-                                        assume_straight_pages=False)
-            self.__model = model
+            self.__config = config
+
+        return self.__config
+
+    @property
+    def model(self) -> OpenDetector:
+        if self.__model is None:
+            self.__model = OpenDetector(self.config)
 
         return self.__model
 
-    def preprocess_image(self,
-                         image: Image) -> np.ndarray:
+    def preprocess_image(self, image: Image) -> np.ndarray:
         return np.array(image)
 
-    def create_result(self,
-                      preds: list[dict],
-                      img_width: int,
-                      img_height: int) -> DetectionPredictionResult:
-
+    def create_result(self, preds: list[dict]) -> DetectionPredictionResult:
         ids = []
         points = []
 
-        # Process bboxes, ids and append to lists
-        bboxes = preds[0]["words"]
-        for bbox in bboxes:
-            bbox = bbox[:4] * np.array([img_width, img_height])
-            bbox = bbox.astype(np.int32)
-            bbox = bbox.flatten().tolist()
+        bboxes = preds[0]["boxes"]
 
+        # Process bboxes, ids and append to lists
+        for bbox in bboxes:
+            bbox = bbox.astype(np.int32).flatten().tolist()
             ids.append(0)
             points.append(bbox)
 
         # Create result
-        result = DetectionPredictionResult(ids=ids,
-                                           points=points,
-                                           id2label={0: "text"})
+        result = DetectionPredictionResult(ids=ids, points=points, id2label={0: "text"})
 
         return result
 
-    def predict(self,
-                image: Image) -> DetectionPredictionResult:
+    def predict(self, image) -> DetectionPredictionResult:
         img = self.preprocess_image(image)
-        img_height, img_width, _ = img.shape
-        preds = self.model([img])
-        result = self.create_result(preds, img_width, img_height)
-
+        preds = self.model(img_numpy=img)
+        result = self.create_result(preds)
         return result
