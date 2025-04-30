cb5a0a493597f0421ff6938b84ed9c467eabfbe1
seblful <alesha.chimba@gmail.com>
2025-04-30 17:42:01 +0300

    [EDIT] delete crop_image

--- src/digitex/core/utils.py
+++ src/digitex/core/utils.py
@@ -1,44 +1,7 @@
 import os
 from PIL import Image
 
-import numpy as np
-import cv2
-
 from digitex.core.handlers.pdf import PDFHandler
-
-
-def crop_image(
-    image: Image.Image, points: list[float], offset: float = 0.025
-) -> Image.Image:
-    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    height, width = img.shape[:2]
-
-    pts = np.array(points)
-    rect = cv2.boundingRect(pts)
-    x, y, w, h = rect
-    img = img[y : y + h, x : x + w].copy()
-
-    pts = pts - pts.min(axis=0)
-    mask = np.zeros(img.shape[:2], np.uint8)
-    cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-
-    result = cv2.bitwise_and(img, img, mask=mask)
-    bg = np.ones_like(img, np.uint8) * 255
-    cv2.bitwise_not(bg, bg, mask=mask)
-    result = bg + result
-
-    border = int(height * offset)
-    result = cv2.copyMakeBorder(
-        result,
-        border,
-        border,
-        border,
-        border,
-        cv2.BORDER_CONSTANT,
-        value=[255, 255, 255],
-    )
-
-    return Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 
 
 def create_pdf_from_images(image_dir: str, output_dir: str) -> None:
