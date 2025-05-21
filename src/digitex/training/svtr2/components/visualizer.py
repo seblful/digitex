09d9e4954d32ccc5f2089fdd35808852867f9ff8
seblful <alesha.chimba@gmail.com>
2025-05-21 19:27:38 +0300

    [EDIT] refactor with base class

--- src/digitex/training/svtr2/components/visualizer.py
+++ src/digitex/training/svtr2/components/visualizer.py
@@ -1,14 +1,18 @@
 import os
 import random
 from typing import Optional
+from abc import ABC, abstractmethod
 
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 
 from digitex.core.processors.file import FileProcessor
 
+import lmdb
+import io
 
-class Visualizer:
+
+class BaseVisualizer(ABC):
     def __init__(
         self, dataset_dir: str, check_images_dir: str, font_path: Optional[str] = None
     ) -> None:
@@ -33,17 +37,6 @@ class Visualizer:
                 self._font = ImageFont.load_default()
         return self._font
 
-    def _get_data(self, set_dir: str) -> tuple[list[str], list[str]]:
-        image_paths, texts = [], []
-        labels_txt_path = os.path.join(set_dir, "labels.txt")
-        lines = FileProcessor.read_txt(labels_txt_path, strip=True)
-        for line in lines:
-            dst_image_path, text = line.split("\t", maxsplit=1)
-            image_path = os.path.join(set_dir, dst_image_path)
-            image_paths.append(image_path)
-            texts.append(text)
-        return image_paths, texts
-
     def _draw_image(
         self, image: Image.Image, label: str, padding: int = 15
     ) -> Image.Image:
@@ -63,20 +56,6 @@ class Visualizer:
         draw.text((text_x, text_y), label, font=self.font, fill="red")
         return drawn_image
 
-    def visualize(self, num_images: int = 10) -> None:
-        for set_dir in self.dataset_dirs:
-            dir_name = os.path.basename(set_dir)
-            image_paths, texts = self._get_data(set_dir)
-            data_len = len(image_paths)
-            for counter in tqdm(
-                range(num_images), desc=f"Visualizing {dir_name} images"
-            ):
-                rnd_idx = random.randint(0, data_len - 1)
-                image_path, text = image_paths[rnd_idx], texts[rnd_idx]
-                with Image.open(image_path) as image:
-                    drawn_image = self._draw_image(image=image, label=text)
-                    self._save_image(set_dir=set_dir, image=drawn_image, idx=counter)
-
     @staticmethod
     def _find_dir_name(set_dir: str) -> str:
         basename = os.path.basename(set_dir)
@@ -91,3 +70,34 @@ class Visualizer:
         image_path = f"{idx}_{set_dir_name}.jpg"
         save_path = os.path.join(self.check_images_dir, image_path)
         image.save(save_path)
+
+    @abstractmethod
+    def visualize(self, num_images: int = 10) -> None:
+        pass
+
+
+class SimpleVisualizer(BaseVisualizer):
+    def _get_data(self, set_dir: str) -> tuple[list[str], list[str]]:
+        image_paths, texts = [], []
+        labels_txt_path = os.path.join(set_dir, "labels.txt")
+        lines = FileProcessor.read_txt(labels_txt_path, strip=True)
+        for line in lines:
+            dst_image_path, text = line.split("\t", maxsplit=1)
+            image_path = os.path.join(set_dir, dst_image_path)
+            image_paths.append(image_path)
+            texts.append(text)
+        return image_paths, texts
+
+    def visualize(self, num_images: int = 10) -> None:
+        for set_dir in self.dataset_dirs:
+            dir_name = os.path.basename(set_dir)
+            image_paths, texts = self._get_data(set_dir)
+            data_len = len(image_paths)
+            for counter in tqdm(
+                range(num_images), desc=f"Visualizing {dir_name} images"
+            ):
+                rnd_idx = random.randint(0, data_len - 1)
+                image_path, text = image_paths[rnd_idx], texts[rnd_idx]
+                with Image.open(image_path) as image:
+                    drawn_image = self._draw_image(image=image, label=text)
+                    self._save_image(set_dir=set_dir, image=drawn_image, idx=counter)
