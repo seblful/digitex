df7b1923255bff77e88c49ee94e4506ff2488f27
seblful <alesha.chimba@gmail.com>
2025-06-02 20:42:27 +0300

    [EDIT] move to core.utils

--- src/digitex/core/utils.py
+++ src/digitex/core/utils.py
@@ -1,5 +1,8 @@
 import os
+import random
 from PIL import Image
+
+import numpy as np
 
 from digitex.core.handlers.pdf import PDFHandler
 
@@ -23,3 +26,18 @@ def create_pdf_from_images(image_dir: str, output_dir: str) -> None:
     pdf_name = f"{os.path.basename(image_dir)} {os.path.basename(output_dir)}.pdf"
     pdf_path = os.path.join(output_dir, pdf_name)
     PDFHandler().create_pdf(images, pdf_path)
+
+
+def get_random_image(images_dir, images_listdir: list[str]) -> tuple[np.ndarray, str]:
+    image_name = random.choice(images_listdir)
+    image_path = os.path.join(images_dir, image_name)
+    image = Image.open(image_path)
+
+    return image_name, image
+
+
+def get_random_img(images_dir, images_listdir: list[str]) -> tuple[np.ndarray, str]:
+    img_name, image = get_random_image(images_dir, images_listdir)
+    img = np.array(image)
+
+    return img_name, img
