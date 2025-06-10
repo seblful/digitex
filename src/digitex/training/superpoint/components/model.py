acdca62614ef3a2e83fcb644598f4664aaa81497
seblful <alesha.chimba@gmail.com>
2025-06-10 20:45:03 +0300

    [EDIT] create_deconv_layers

--- src/digitex/training/superpoint/components/model.py
+++ src/digitex/training/superpoint/components/model.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -8,9 +10,9 @@ class HeatmapKeypointModel(nn.Module):
         self,
         max_keypoints: int,
         backbone_out_channels: int,
-        deconv_channels: int = 256,
-        num_deconv_layers: int = 3,
-        output_stride: int = 4,
+        backbone_stride: int,
+        deconv_channels: int,
+        output_stride: int,
     ) -> None:
         """
         Heatmap-based keypoint prediction model.
@@ -20,24 +22,49 @@ class HeatmapKeypointModel(nn.Module):
             max_keypoints: Maximum number of keypoints to predict (defines output channels)
             backbone_out_channels: Number of output channels from the backbone
             deconv_channels: Number of channels in deconvolution layers
-            num_deconv_layers: Number of deconvolution layers
             output_stride: Target output stride relative to input (must be power of 2)
         """
         super().__init__()
-        self.backbone = self.create_resnet_backbone(freeze_initial_layers=True)
         self.max_keypoints = max_keypoints
 
         # Verify output_stride is power of 2
         if not (output_stride & (output_stride - 1) == 0) and output_stride != 0:
             raise ValueError("output_stride must be a power of 2")
 
-        # Calculate required deconvolution steps
-        self.deconv_layers = nn.Sequential()
+        # Create backbone
+        self.backbone = self.create_backbone(freeze_initial_layers=True)
+
+        # Create deconvolution layers
+        self.deconv_layers = self.create_deconv_layers(
+            backbone_out_channels, backbone_stride, deconv_channels, output_stride
+        )
+
+        # Create final prediction layer
+        self.final_conv = nn.Conv2d(deconv_channels, max_keypoints, kernel_size=1)
+
+        # # TODO maybe add sigmoid
+        # self.final_conv = nn.Sequential(
+        #     nn.Conv2d(deconv_channels, max_keypoints, kernel_size=1),
+        #     nn.Sigmoid(),  # Ensures heatmap values in [0,1]
+        # )
+
+        # Initialize weights
+        self._initialize_weights()
+
+    def create_deconv_layers(
+        self,
+        backbone_out_channels: int,
+        backbone_stride: int,
+        deconv_channels: int,
+        output_stride: int,
+    ) -> nn.Sequential:
+        deconv_layers = nn.Sequential()
         in_channels = backbone_out_channels
 
         # Add deconvolution blocks
+        num_deconv_layers = int(math.log2(backbone_stride // output_stride))
         for _ in range(num_deconv_layers):
-            self.deconv_layers.append(
+            deconv_layers.append(
                 nn.ConvTranspose2d(
                     in_channels,
                     deconv_channels,
@@ -48,22 +75,13 @@ class HeatmapKeypointModel(nn.Module):
                     bias=False,
                 )
             )
-            self.deconv_layers.append(nn.BatchNorm2d(deconv_channels))
-            self.deconv_layers.append(nn.ReLU(inplace=True))
+            deconv_layers.append(nn.BatchNorm2d(deconv_channels))
+            deconv_layers.append(nn.ReLU(inplace=True))
             in_channels = deconv_channels
 
-        # Final prediction layer
-        self.final_conv = nn.Conv2d(deconv_channels, max_keypoints, kernel_size=1)
-        # # TODO maybe add sigmoid
-        # self.final_conv = nn.Sequential(
-        #     nn.Conv2d(deconv_channels, max_keypoints, kernel_size=1),
-        #     nn.Sigmoid(),  # Ensures heatmap values in [0,1]
-        # )
+        return deconv_layers
 
-        # Initialize weights
-        self._initialize_weights()
-
-    def create_resnet_backbone(
+    def create_backbone(
         self,
         freeze_initial_layers: bool = True,
     ) -> nn.Module:
