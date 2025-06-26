0fb582a0601e9a51b31406175b343b8fd269e861
seblful <alesha.chimba@gmail.com>
2025-06-26 19:55:42 +0300

    [EDIT] train utils

--- src/digitex/training/segformer/components/utils.py
+++ src/digitex/training/segformer/components/utils.py
@@ -1,3 +1,10 @@
+import yaml
+
+import torch
+from transformers import SegformerForSemanticSegmentation
+
+from digitex.settings import settings
+
 from digitex.training.superpoint.components.annotation import (
     RelativeKeypoint,
     AbsoluteKeypoint,
@@ -66,3 +73,42 @@ def create_rel_kps_obj_from_label(
     )
 
     return kps_obj
+
+
+def load_config(config_path: str) -> dict:
+    with open(config_path, "r") as file:
+        return yaml.safe_load(file)
+
+
+def load_model(config: dict) -> SegformerForSemanticSegmentation:
+    # Load model
+    model = SegformerForSemanticSegmentation.from_pretrained(
+        config["model"]["model_name"],
+        num_labels=config["dataset"]["num_classes"],
+        id2label=config["model"]["id2label"],
+        label2id=config["model"]["label2id"],
+        ignore_mismatched_sizes=True,
+    )
+
+    # Load checkpoint
+    checkpoint = torch.load(
+        config["trainer"]["checkpoint_path"],
+        map_location=settings.DEVICE,
+        weights_only=False,
+    )
+
+    # Load model state dict
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.to(settings.DEVICE)
+    model.eval()
+
+    return model
+
+
+def save_model(model: SegformerForSemanticSegmentation, config: dict) -> None:
+    model.save_pretrained(config["trainer"]["checkpoint_dir"])
+
+
+def convert_model(config: dict) -> None:
+    model = load_model(config)
+    save_model(model, config)
